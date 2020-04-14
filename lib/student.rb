dade6447dafcb42a6ecc74068fab94a68136aea7
Elijah wines <elijahwines84@yahoo.com>
2020-04-14 16:23:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,8 +72,8 @@ class Student
   end
 
   def self.students_below_12th_grade
-    sql = <<-SQL 
-      SELECT * FROM students 
+    sql = <<-SQL
+      SELECT * FROM students
       WHERE grade < 12
     SQL
 
